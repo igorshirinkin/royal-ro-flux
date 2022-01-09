abde89cdb610152f7ba9f31ed669f8becfecd136
igorshirinkin <shoutaim92@gmail.com>
2022-01-09 12:38:01 +0500

    SDEnableCreditRewards to false

--- config/application.php
+++ config/application.php
@@ -213,7 +213,7 @@ return array(
 	'FontResolvedColour'		=> 'green',
 	'FontPendingColour'			=> 'orange',
 	'FontClosedColour'			=> 'darkgrey',
-	'SDEnableCreditRewards'		=> true,					// Show option in Service Desk to reward player X credits for reporting bugs/abuse/etc.
+	'SDEnableCreditRewards'		=> false,					// Show option in Service Desk to reward player X credits for reporting bugs/abuse/etc.
 	'SDCreditReward'			=> 10,						// Number of credits to award account.
 
 	// Discord Webhooks
