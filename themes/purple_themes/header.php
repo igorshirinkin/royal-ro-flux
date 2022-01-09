bb9c300ab9683ad1a2ea9c2497cd45b2507ab053
igorshirinkin <shoutaim92@gmail.com>
2022-01-09 15:07:32 +0500

    Fix

--- themes/purple_themes/header.php
+++ themes/purple_themes/header.php
@@ -1,6 +1,7 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <?php require_once(FLUX_THEME_DIR . "/purple_themes/skyzone_config.php"); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!DOCTYPE html>
 <html lang="ru">
 
 <head>
