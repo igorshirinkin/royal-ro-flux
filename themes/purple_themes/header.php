b7d030676b534ae6de753734d602dc62a998ac4c
igorshirinkin <shoutaim92@gmail.com>
2022-01-10 21:07:27 +0300

    Deleted FA

--- themes/purple_themes/header.php
+++ themes/purple_themes/header.php
@@ -43,7 +43,6 @@
 
 <body id="top">
 
-	<?php echo "<i class=\"far fa-user\"></i>"; ?>
 	<!-- uses solid style -->
 	<i class="far fa-user"></i> <!-- uses regular style -->
 	<i class="fal fa-user"></i> <!-- uses light style -->
